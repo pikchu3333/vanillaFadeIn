fbc4aa235bc3639b640922944496e9beafa0bf2f
pikchu3333 <57980360+pikchu3333@users.noreply.github.com>
2022-03-08 13:11:33 +0900

    Add files via upload
    kkk

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# vanillaFadeIn
+# Vanilla fadeIn
+
+`Jquery`없이 페이드인 기능을 사용하는 분들을 위한 스크립트 입니다.
+별도의 CSS나 라이브러리가 전혀 필요 없습니다.
+
+[Demo](https://www.gloomy-store.com/demo/vanillaFadeIn/)
+
+### CDN
+
+javascript
+```html
+<script src="https://cdn.gloomy-store.com/vanillaFadeIn/vanillaFadeIn.js"></script>
+```
+
+## Type
+
+두 가지 타입으로 사용이 가능합니다.
+1. `단독사용`
+2. `화면에 나타나면 작동`
+
+1. 
+원하는 때에 `fadeIn(element)`를 기재하여 fadeIn 함수를 호출합니다.
+ex) `fadeIn(document.getElementById('element1'))`
+
+2.
+fadeIn을 원하는 요소에 `js-fade-in`클래스를 붙여놓습니다.
+
+```html
+<div class="js-fade-in" style="opacity:0;"></div>
+```
+해당 클래스를 붙일 경우, 화면 높이를 계산해서 
+화면에서 스크롤이 벗어날 경우 요소가 fadeOut되며,
+화면 안으로 들어올 경우 요소가 fadeIn됩니다.
+
+## Property
+
+모든 속성은 `data-sa-*`로 사용합니다.
+
+```html
+<div
+    data-sa-offset="200px"
+    data-sa-delay="50"
+    data-sa-duration="1000"
+    data-sa-once="false"
+>
+</div>
+```
+
+
+### fadeOut
+
+fadeIn과 사용방법은 동일합니다.
+```html
+<sctipt>
+	window.addEventListener('load',function(){
+		fadeOut(document.getElementById('element1'))
+	})
+</sctipt>
+```
+해당 앨리먼트의 애니메이션이 스크롤에 따라 `반복`됩니다.
+
+`기본값은 true`이며, `boolean`입니다.
---
+++ vanillaFadeIn.js
@@ -0,0 +1,63 @@
+/*!
+		 * Vanila FadeIn
+		 * https://gloomy-store.com
+		 *
+		 * Copyright OpenJS Foundation and other contributors
+		 * Released under the MIT license
+		 * 
+		 * Made In Gloomy-store
+		 *
+		 * Date: 2022-03-08
+		 */
+		
+		/*fadeIn*/
+		function fadeIn(t) {
+			let op = Number(t.style.opacity);
+			if(op < 1 && op >0.1) return;
+			let Timer = 1000;
+			setInterval(function() {
+				if (op < 1) {
+					op = Number((op + 0.025).toFixed(3));
+					t.style.opacity = op;
+				} else if (op > 1) {
+					op = 1
+				} else return
+			}, (Timer / 40))
+		}
+
+		/*fadeOut*/
+		function fadeOut(t) {
+			let op = Number(t.style.opacity);
+			if (!op && op !== 0) op = 1;
+			if(op < 1 && op >0.1) return;
+			let Timer = 800;
+			setInterval(function() {
+				if (op > 0) {
+					op = Number((op - 0.025).toFixed(3));
+					if (op < 0.025) op = 0;
+					t.style.opacity = op;
+				} else if (op < 0) {
+					op = 0
+				} else return
+			}, (Timer / 40))
+		}
+
+		let h = window.outerHeight;
+		window.addEventListener('scroll', function() {
+			let jsFadeIn = document.getElementsByClassName('js-fade-in')
+			for (let i = 0; i < jsFadeIn.length; i++) {
+				if (jsFadeIn[i].getBoundingClientRect().top < h && jsFadeIn[i].getBoundingClientRect().top > -200) fadeIn(jsFadeIn[i])
+				else {
+					fadeOut(jsFadeIn[i])
+				}
+			}
+
+		})
+		window.addEventListener('load', function() {
+			let jsFadeIn = document.getElementsByClassName('js-fade-in')
+			
+			for (let i = 0; i < jsFadeIn.length; i++) {
+				jsFadeIn[i].style.opacity = 0.1;
+				if (jsFadeIn[i].getBoundingClientRect().top < h && jsFadeIn[i].getBoundingClientRect().top > -200) fadeIn(jsFadeIn[i])
+			}
+		})
